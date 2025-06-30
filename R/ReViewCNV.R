824b3b786a2f6883775ce8e21f4c76a431256449
Francesco <francescopaparazzo85@gmail.com>
2025-06-30 16:07:06 +0200

    Bug fixed

--- R/ReViewCNV.R
+++ R/ReViewCNV.R
@@ -4,7 +4,7 @@
 #'
 #' @param host The IPv4 address that the application should listen
 #' @param port The TCP port that the application should listen on
-#' @param launch.browser If true, the system's default web browser will be launched automatically after the app is started.
+#' @param launch If true, the system's default web browser will be launched automatically after the app is started
 #'
 #' @returns  Runs a Shiny application in the default web browser.
 #' @export
@@ -14,7 +14,7 @@
 #'   Plot_Visualization()
 #' }
 
-ReViewCNV <- function(host="0.0.0.0", port = 3838, launch.browser = TRUE ) {
+ReViewCNV <- function(host="0.0.0.0", port = 3838, launch = TRUE ) {
 # Specify the application port
 options(shiny.maxRequestSize=50*1024^2) #max dim for input files
 
@@ -2485,6 +2485,6 @@ if(!is.null(exons_annotations())){
 }
 
   app <- shiny::shinyApp(ui = ui, server = server)
-  shiny::runApp(app, host = host, port = port)
+  shiny::runApp(app, host = host, port = port, launch.browser = launch)
 
 }
